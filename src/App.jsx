84a2993dad3ec5c696e684f82229074c86de444b
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2024-01-18 07:16:07 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,20 @@ import BChanger from "./apps/2/BChanger";
 import ParaGenerator from "./apps/4/ParaGenerator";
 import GitHub from "./apps/5/GitHub";
 import OTP from "./apps/6/OTP";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Switch, Router, Route } from "react-router-dom";
 import Links from "./Links";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Links />}></Route>
-          <Route path="/1" element={<Profile />}></Route>
-          <Route path="/2" element={<BChanger />}></Route>
-          <Route path="/4" element={<ParaGenerator />}></Route>
-          <Route path="/5" element={<GitHub />}></Route>
-          <Route path="/6" element={<OTP />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <Switch>
+          <Route path="/" exact Component={Links}></Route>
+          <Route path="/1" Component={Profile}></Route>
+          <Route path="/2" Component={BChanger}></Route>
+          <Route path="/5" Component={ParaGenerator}></Route>
+          <Route path="/6" Component={OTP}></Route>
+        </Switch>
+      </Router>
     </>
   );
 }
