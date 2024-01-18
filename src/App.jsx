af451bfb72609e50f50c64f63ae60e40425a6c81
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2024-01-18 07:08:59 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import BChanger from "./apps/2/BChanger";
 import ParaGenerator from "./apps/4/ParaGenerator";
 import GitHub from "./apps/5/GitHub";
 import OTP from "./apps/6/OTP";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Links from "./Links";
-
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Links />} />
-          <Route path="/1" element={<Profile />} />
-          <Route path="/2" element={<BChanger />} />
-          <Route path="/4" element={<ParaGenerator />} />
-          <Route path="/5" element={<GitHub />} />
-          <Route path="/6" element={<OTP />} />
+          <Route path="/" element={<Links />}></Route>
+          <Route path="/1" element={<Profile />}></Route>
+          <Route path="/2" element={<BChanger />}></Route>
+          <Route path="/4" element={<ParaGenerator />}></Route>
+          <Route path="/5" element={<GitHub />}></Route>
+          <Route path="/6" element={<OTP />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/Links.jsx
+++ src/Links.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useNavigate } from "react-router-dom";
 
 function LinkBtn({ num }) {
   return (
