f82aa3eb7af9ad2fffb96684b270f7c61805e92d
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2024-01-18 07:35:10 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,44 @@ import BChanger from "./apps/2/BChanger";
 import ParaGenerator from "./apps/4/ParaGenerator";
 import GitHub from "./apps/5/GitHub";
 import OTP from "./apps/6/OTP";
-import { Switch, Router, Route } from "react-router-dom";
-import Links from "./Links";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import "./App.css";
+
+function LinkBtn({ num, navigate }) {
+  return (
+    <a onClick={() => navigate("/" + num)} id="link-btn">
+      {num}
+    </a>
+  );
+}
+function Links() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    document.body.style.backgroundColor = "white";
+  });
+  return (
+    <div id="links">
+      {[1, 2, 4, 5, 6].map((n, i) => (
+        <LinkBtn num={n} key={i} navigate={navigate} />
+      ))}
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route path="/" exact Component={Links}></Route>
-          <Route path="/1" Component={Profile}></Route>
-          <Route path="/2" Component={BChanger}></Route>
-          <Route path="/5" Component={ParaGenerator}></Route>
-          <Route path="/6" Component={OTP}></Route>
-        </Switch>
-      </Router>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Links />}></Route>
+          <Route path="/1" element={<Profile />}></Route>
+          <Route path="/2" element={<BChanger />}></Route>
+          <Route path="/4" element={<ParaGenerator />}></Route>
+          <Route path="/5" element={<GitHub />}></Route>
+          <Route path="/6" element={<OTP />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
